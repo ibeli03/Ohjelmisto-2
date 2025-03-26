5bdc0e6128f69f0a179bcda38e130b10f91bee56
ibeli03 <isabel.aldanakekoni@metropolia.fi>
2025-03-26 11:45:31 +0200

    h

--- tehtava_9.4.py
+++ tehtava_9.4.py
@@ -18,37 +18,48 @@ class Car:
     def drive(self, hours):
         self.travelled_distance += self.current_speed * hours
 
+class Race:
+    def __init__(self, name, distance, cars):
+        self.name = name
+        self.distance = distance
+        self.cars = cars
+    def hour_passes(self):
+        for car in self.cars:
+            car.accelerate()
+            car.drive()
+    def print_status(self):
+        print(f"\nStatus of {self.name} Race: ")
+        print(f"{'Registration':<15}{'Max speed':<12}{'Distance traveled':<20}")
+        print("-" * 50)
+
+        for car in self.cars:
+            print(f"{car.registration_number:<15}{car.max_speed:<12}{car.distance_travelled:<20}")
+    def race_finished(self):
+        for car in self.cars:
+            if car.distance_travelled >= self.distance:
+                return False
+            return False
+
 
 cars = []
 
 for i in range(1, 11):
     max_speed = random.randint(100, 200)
     registration_number = f"ABC-{i}"
-    car = Car(registration_number, max_speed)
-    cars.append(car)
+    cars.append(Car(registration_number, max_speed))
+race = Race("Grand demolition Derby", 8000, cars)
 
-race_over = False
-hour = 0
+hours = 0
+while not race.race_finished():
+    race.hour_passes()
+    hours += 1
 
-while not race_over:
-    hour += 1
-for car in cars:
-        speed_change = random.randint(-10, 15)
-        car.accelerate(speed_change)
-        car.drive(1)
+    if hours % 10 == 0:
+        race.print_status()
 
-for car in cars:
-    if car.travelled_distance >= 10000:
-            race_over = True
-            winner = car
-            break
+race.print_status()
+print(f"\nRace finished after {hours} hours!")
 
-print(f"Race Over! The winner is {winner.registration_number}!")
-print("\nFinal standings:")
-print(f"{'Registration Number':<15} {'Max Speed (km/h)':<15} {'Current Speed (km/h)':<20} {'Travelled Distance (km)'}")
-
-for car in cars:
-    print(f"{car.registration_number:<15} {car.max_speed:<15} {car.current_speed:<20} {car.travelled_distance}")
 
 
 
